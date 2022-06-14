cbec768089727c59d6f4f39afd5929e9335481c7
francesco pastore <pastore@promete.it>
2022-06-14 16:27:22 +0200

    ci siamo quasi

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+mod polyregres;
+
+use polyregres::{DataPoint, PolynomialRegression};
+
 use eframe::egui;
 use egui::plot::{Line, Plot, Points, Value, Values};
 use std::io::BufRead;
@@ -20,14 +24,28 @@ struct Point {
    delta: i32,
 }
 
+struct Event {
+   time: u32,
+   name: String,
+}
+
 struct MyApp {
    data_loaded: bool,
    data: Vec<Point>,
+   regression: Vec<Value>,
    min_limit_fq: u32,
    max_limit_fq: u32,
    min_fq: u32,
    max_fq: u32,
    delta: u32,
+   show_regression: bool,
+   show_by_event: bool,
+   show_interpolated: bool,
+   regression_grade: String,
+   events: Vec<Event>,
+   min: String,
+   sec: String,
+   name: String,
 }
 
 impl MyApp {
@@ -39,6 +57,7 @@ impl MyApp {
          return;
       }
       self.data.clear();
+      self.regression.clear();
       let mut time: f32 = 5.1;
       let f = std::io::BufReader::new(f.unwrap());
       for line in f.lines() {
@@ -54,6 +73,7 @@ impl MyApp {
                   y: fq,
                   delta: 0,
                });
+
                if fq < minfq {
                   minfq = fq;
                }
@@ -64,12 +84,36 @@ impl MyApp {
             time += 5.1;
          }
       }
-      println!("{minfq} {maxfq}");
+      //println!("{minfq} {maxfq}");
       self.min_limit_fq = minfq;
       self.max_limit_fq = maxfq;
       self.min_fq = minfq;
       self.max_fq = maxfq;
+
+      // calcolo la regressione totale
+      self.calculate_regression();
+
       self.data_loaded = true;
+   }
+
+   fn calculate_regression(&mut self) {
+      
+      let grado = self.regression_grade.parse::<usize>().unwrap_or(1);
+      let data = self
+         .data
+         .iter()
+         .map(|p| DataPoint {
+            x: p.x as f64,
+            y: p.y as f64,
+         })
+         .collect();
+      let mut poly = PolynomialRegression::new(data, grado);
+      let terms = poly.get_terms();
+      self.regression = self
+         .data
+         .iter()
+         .map(|p| Value::new(p.x, PolynomialRegression::predictY(&terms, p.x as f64)))
+         .collect();
    }
 }
 
@@ -78,11 +122,21 @@ impl Default for MyApp {
       Self {
          data_loaded: false,
          data: Vec::new(),
+         regression: Vec::new(),
          min_fq: 0,
          max_fq: 0,
          delta: 0,
          min_limit_fq: 0,
          max_limit_fq: 100,
+         show_regression: false,
+         show_by_event: false,
+         show_interpolated: false,
+         regression_grade: String::from("3"),
+         events: Vec::new(),
+         min: String::new(),
+         sec: String::new(),
+         name: String::new(),
+
       }
    }
 }
@@ -104,7 +158,7 @@ impl eframe::App for MyApp {
          });
          ui.add_space(10.0);
          ui.heading("Limiti");
-         egui::Grid::new("some_unique_id").show(ui, |ui| {
+         egui::Grid::new("sliders").show(ui, |ui| {
             ui.label("Min:");
             if ui
                .add(
@@ -126,6 +180,55 @@ impl eframe::App for MyApp {
             ui.add(egui::Slider::new(&mut self.delta, 0..=100).show_value(false));
             ui.end_row();
          });
+         ui.add_space(10.0);
+         ui.separator();
+         ui.heading("Regressione");
+         //ui.add_space(10.0);
+         ui.horizontal(|ui| {
+            ui.checkbox(&mut self.show_regression, "Globale di grado");
+            let response = ui.add(egui::TextEdit::singleline(&mut self.regression_grade));
+            if response.changed() {
+               // println!("{}",self.regression_grade);
+               self.regression_grade.retain(|c| c.is_ascii_digit());
+               if self.regression_grade.len() > 2 {
+                  self.regression_grade.pop();
+               }
+              
+               self.calculate_regression();
+            }
+            //if response.lost_focus() && ui.input().key_pressed(egui::Key::Enter) {
+            // …
+            //}
+         });
+         ui.checkbox(&mut self.show_by_event, "Per fase");
+         ui.checkbox(&mut self.show_interpolated, "Per fase interpolata");
+         ui.add_space(10.0);
+         ui.separator();
+         ui.heading("Eventi");
+         ui.vertical_centered(|ui| {
+            let button = egui::Button::new("Default"); //.fill(egui::Color32::from_rgb(0x33, 0x55, 0xcc)).stroke(stroke);
+
+            if ui.add_sized([70.0, 25.0], button).clicked() {
+               // imposta gli eventi di default
+            }
+         });
+
+         egui::Grid::new("eventi").show(ui, |ui| {});
+         ui.horizontal(|ui| {
+            // ui.text_edit_singleline(&mut self.min);
+           // ui.add_sized([40.0, 20.0], egui::TextEdit::singleline::new(&mut self.min));
+            ui.add_sized([40.0,20.0], egui::TextEdit::singleline(&mut self.min));
+            ui.label(":");
+            ui.add_sized([40.0,20.0], egui::TextEdit::singleline(&mut self.sec));
+            ui.add_sized([80.0,20.0], egui::TextEdit::singleline(&mut self.name));
+            if ui.button("add").clicked(){
+
+            }
+         });
+
+
+
+
       });
 
       egui::CentralPanel::default().show(ctx, |ui| {
@@ -135,8 +238,17 @@ impl eframe::App for MyApp {
                .iter()
                .filter(|p| p.y >= self.min_fq && p.y <= self.max_fq)
                .map(|p| Value::new(p.x, p.y));
+            let reg = Line::new(Values::from_values_iter(
+               self.regression.iter().map(|p| Value::new(p.x, p.y)),
+            )).width(4.0);
             let punti = Points::new(Values::from_values_iter(serie)).radius(4.0);
-            Plot::new("my_plot").show(ui, |plot_ui| plot_ui.points(punti));
+            let p = Plot::new("my_plot");
+            p.show(ui, |plot_ui| {
+               plot_ui.points(punti);
+               if self.show_regression {
+                  plot_ui.line(reg);
+               }
+            });
          }
       });
    }
---
+++ src/polyregres.rs
@@ -0,0 +1,223 @@
+mod matrix {
+
+    use super::Matrix;
+
+    pub fn gaussian_jordan_elimination(left_matrix: &Matrix, right_matrix: &Vec<f64>) -> Vec<f64> {
+        let combined = combine_matrices(left_matrix, right_matrix);
+        let mut fwd_integration = forward_elimination(&combined);
+        let l1 = (fwd_integration.len() - 1) as i32;
+        let l2 = (fwd_integration[0].len() - 2) as i32;
+        let mut arr = vec![0.0;fwd_integration.len()];
+    
+        //println!("{l1} {l2}");
+        //NOW, FINAL STEP IS BACKWARD SUBSTITUTION WHICH RETURNS THE TERMS NECESSARY FOR POLYNOMIAL REGRESSION
+        backward_substitution(&mut fwd_integration, &mut arr, l1, l2).to_vec()
+    }
+    
+    fn combine_matrices(left: &Matrix, right: &Vec<f64>) -> Matrix {
+        let rows = right.len();
+        let cols = left[0].len();
+        let mut return_matrix = Vec::new();
+    
+        for i in 0..rows {
+            return_matrix.push(Vec::new());
+    
+            for j in 0..=cols {
+                if j == cols {
+                    return_matrix[i].push(right[i]);
+                } else {
+                    return_matrix[i].push(left[i][j]);
+                }
+            }
+        }
+    
+        return_matrix
+    }
+    
+    fn forward_elimination(any_matrix: &Matrix) -> Matrix {
+        let rows = any_matrix.len();
+        let cols = any_matrix[0].len();
+        let mut matrix = Vec::new();
+        //return_matrix = any_matrix;
+        for i in 0..rows {
+            matrix.push(Vec::new());
+    
+            for j in 0..cols {
+                matrix[i].push(any_matrix[i][j]);
+            }
+        }
+    
+        for x in 0..rows - 1 {
+            let mut z = x;
+            while z < rows - 1 {
+                let numerator = matrix[z + 1][x];
+                let denominator = matrix[x][x];
+                let result = numerator / denominator;
+    
+                for i in 0..cols {
+                    matrix[z + 1][i] = matrix[z + 1][i] - (result * matrix[x][i]);
+                }
+                z += 1;
+            }
+        }
+        matrix
+    }
+    
+    fn backward_substitution<'a>(
+        any_matrix: &mut Matrix,
+        arr: &'a mut Vec<f64>,
+        row: i32,
+        col: i32,
+    ) -> &'a Vec<f64> {
+        if row < 0 || col < 0 {
+            return arr;
+        } else {
+            //println!("{row} {col}");
+            let col = col as usize;
+            let row = row as usize;
+            let rows = any_matrix.len();
+            let cols = any_matrix[0].len() - 1;
+            let mut current = 0.0;
+            let mut counter = 0;
+            let mut i = (cols - 1) as i32;
+            while i >= col as i32 {
+                if i == col as i32 {
+                    current = any_matrix[row][cols] / any_matrix[row][i as usize];
+                } else {
+                    any_matrix[row][cols] -= any_matrix[row][i as usize] * arr[rows - 1 - counter];
+                    counter += 1;
+                }
+                //println!("{i}");
+                i -= 1;
+               
+            }
+    
+            arr[row] = current;
+            return backward_substitution(any_matrix, arr, row as i32 - 1, col as i32 - 1);
+        }
+    }
+}
+
+pub type Matrix = Vec<Vec<f64>>;
+#[derive(Debug)]
+pub struct DataPoint {
+    pub x: f64,
+    pub y: f64,
+}
+#[derive(Debug)]
+pub struct PolynomialRegression {
+    pub data: Vec<DataPoint>,
+    pub degree: usize,
+    pub matrix: Matrix,
+    pub left_matrix: Matrix,
+    pub right_matrix: Vec<f64>,
+}
+
+impl PolynomialRegression {
+    pub fn new(data_points: Vec<DataPoint>, degree: usize) -> Self {
+        PolynomialRegression {
+            data: data_points,
+            degree,
+            matrix: Vec::new(),
+            left_matrix: Vec::new(),
+            right_matrix: Vec::new(),
+        }
+    }
+
+    /**
+     * Sums up all x coordinates raised to a power
+     * @param anyData
+     * @param power
+     * @returns {number}
+     */
+
+    fn sumX(&self, power: i32) -> f64 {
+        self.data.iter().map(|d| d.x.powi(power)).sum()
+    }
+
+    /**
+     * sums up all x * y where x is raised to a power
+     * @param anyData
+     * @param power
+     * @returns {number}
+     */
+    fn sumXTimesY(&self, power: i32) -> f64 {
+        self.data.iter().map(|d| d.x.powi(power) * d.y).sum()
+    }
+
+    /**
+     * Sums up all Y's raised to a power
+     * @param anyData
+     * @param power
+     * @returns {number}
+     */
+    fn sumY(&self, power: i32) -> f64 {
+        self.data.iter().map(|d| d.y.powi(power)).sum()
+    }
+
+    /**
+     * generate the left matrix
+     */
+    fn generateLeftMatrix(&mut self) {
+        for i in 0..=self.degree {
+            self.left_matrix.push(Vec::new());
+            for j in 0..=self.degree {
+                if i == 0 && j == 0 {
+                    self.left_matrix[i].push(self.data.len() as f64);
+                } else {
+                    let v = self.sumX((i + j) as i32);
+                    self.left_matrix[i].push(v);
+                }
+            }
+        }
+    }
+    /**
+     * generates the right hand matrix
+     */
+    fn generateRightMatrix(&mut self) {
+        /*
+        for (let i = 0; i <= this.degree; i++) {
+            if (i === 0) {
+                this.rightMatrix[i] = this.sumY(this.data, 1);
+            } else {
+                this.rightMatrix[i] = this.sumXTimesY(this.data, i);
+            }
+        }
+        */
+        for i in 0..=self.degree {
+            if i == 0 {
+                self.right_matrix.push(self.sumY(1) as f64);
+            } else {
+                self.right_matrix.push(self.sumXTimesY(i as i32) as f64);
+            }
+        }
+    }
+    /**
+     * gets the terms for a polynomial
+     * @returns {*}
+     */
+    pub fn get_terms(&mut self) -> Vec<f64> {
+        self.generateLeftMatrix();
+        self.generateRightMatrix();
+        matrix::gaussian_jordan_elimination(&self.left_matrix, &self.right_matrix)
+    }
+    /**
+     * Predicts the Y value of a data set based on polynomial coefficients and the value of an independent variable
+     * @param terms
+     * @param x
+     * @returns {number}
+     */
+    pub fn predictY(terms: &Vec<f64>, x: f64) -> f64 {
+        let mut result = 0.0;
+        let mut i = (terms.len() - 1) as i32;
+        while i >= 0 {
+            if i == 0 {
+                result += terms[i as usize];
+            } else {
+                result += terms[i as usize] * f64::powi(x, i);
+            }
+            i -= 1;
+        }
+        result
+    }
+}
